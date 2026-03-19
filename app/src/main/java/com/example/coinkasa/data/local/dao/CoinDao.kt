a86549cf6692623554028bd102a39a6740000f14
odabasialaaddin <byyodabasi@gmail.com>
2026-03-19 05:12:16 +0300

    feat: setup Room database and DAOs

---
+++ app/src/main/java/com/example/coinkasa/data/local/dao/CoinDao.kt
@@ -0,0 +1,20 @@
+package com.example.coinkasa.data.local.dao
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.coinkasa.data.local.entity.CoinEntity
+
+@Dao
+interface CoinDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(coins: List<CoinEntity>)
+
+    @Query("SELECT * FROM coin_table")
+    fun getCoinsPagingSource(): PagingSource<Int, CoinEntity>
+
+    @Query("DELETE FROM coin_table")
+    suspend fun clearAllCoins()
+}
---
+++ app/src/main/java/com/example/coinkasa/data/local/dao/TransactionDao.kt
@@ -0,0 +1,25 @@
+package com.example.coinkasa.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.coinkasa.data.local.entity.TransactionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TransactionDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTransaction(transaction: TransactionEntity)
+
+    @Delete
+    suspend fun deleteTransaction(transaction: TransactionEntity)
+
+    @Query("SELECT * FROM transaction_table ORDER BY dateMillis DESC")
+    fun getAllTransactions(): Flow<List<TransactionEntity>>
+
+    @Query("SELECT * FROM transaction_table WHERE coinId = :coinId ORDER BY dateMillis DESC")
+    fun getTransactionsByCoinId(coinId: String): Flow<List<TransactionEntity>>
+}
